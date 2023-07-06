a34ea57b0fe9cee9d8df7d19fdeac1cc6a215589
Joe Jarvis <joe.jarvis109@gmail.com>
2023-07-05 20:18:11 -0400

    Add options and caches

--- src/js/OptionsInterface.ts
+++ src/js/OptionsInterface.ts
@@ -1,11 +1,30 @@
 import browser from "webextension-polyfill";
 
+export interface Caches {
+    cacheFollowed: string[]
+    cacheTrending: string[]
+}
+
 export interface Options {
     instance: string
+    replacePublishWithToot: boolean
+    addAltTextPopup: boolean
+    coloredFollowedHashtags: boolean
+    followedColor: string
+    coloredTrendingHashtags: boolean
+    trendingColor: string
+    coloredLGBTHashtags: boolean
 }
 
 export const defaultOptions: Options = {
-    instance: "https://mastodon.social"
+    instance: "https://mastodon.social",
+    replacePublishWithToot: true,
+    addAltTextPopup: true,
+    coloredFollowedHashtags: false,
+    followedColor: "lightgreen",
+    coloredTrendingHashtags: false,
+    trendingColor: "violet",
+    coloredLGBTHashtags: true
 };
 
 export async function GetOptions(): Promise<Options> {
@@ -13,4 +32,13 @@ export async function GetOptions(): Promise<Options> {
         ...defaultOptions,
         ...((await browser.storage.local.get("options")).options ?? {})
     };
+}
+
+export async function GetCaches(): Promise<Caches> {
+    const res = {
+        ...{ cacheFollowed: [], cacheTrending: [] },
+        ...(await browser.storage.local.get(["cacheFollowed", "cacheTrending"]))
+    };
+
+    return res;
 }
