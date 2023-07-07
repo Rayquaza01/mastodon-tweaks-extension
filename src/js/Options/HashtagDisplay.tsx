bd7230334d771a7528315d3b5d9c95a463570c02
Joe Jarvis <joe.jarvis109@gmail.com>
2023-07-07 00:28:06 -0400

    Move display to a separate component

---
+++ src/js/Options/HashtagDisplay.tsx
@@ -0,0 +1,45 @@
+import { HashtagHighlightMode } from "../OptionsInterface";
+import React from "react";
+
+export interface HashtagListProps {
+    visible: boolean;
+    mode: HashtagHighlightMode
+    color: string
+    background: string
+    hashtags: string[]
+}
+
+export function HashtagList(props: HashtagListProps) {
+    return (
+        <div className="hashtagList" style={{ display: props.visible ? undefined : "none" }}>
+            { props.hashtags.map(item => <Hashtag tag={item} mode={props.mode} color={props.color} background={props.background} key={item} />) }
+        </div>
+    );
+}
+
+export interface HashtagProps {
+    tag: string
+    mode: HashtagHighlightMode
+    color: string
+    background: string
+}
+
+export function Hashtag(props: HashtagProps) {
+    const style: React.CSSProperties = {};
+    switch (props.mode) {
+        case HashtagHighlightMode.COLOR:
+            style.color = props.color;
+            break;
+        case HashtagHighlightMode.BACKGROUND:
+            style.background = props.background;
+            style.backgroundClip = "text";
+            style.WebkitTextFillColor = "transparent";
+            break;
+    }
+
+    console.log(props.tag, props.mode);
+
+    return (
+        <span className="hashtag" style={style}>#{props.tag}</span>
+    );
+}
