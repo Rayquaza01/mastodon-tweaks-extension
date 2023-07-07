f5ecc205e34169e6a3290a5a5ba90deb1dd4e9ad
Joe Jarvis <joe.jarvis109@gmail.com>
2023-07-07 11:13:28 -0400

    Add refresh followed to background script

--- src/js/background.ts
+++ src/js/background.ts
@@ -1,17 +1,22 @@
-import browser, { manifest } from "webextension-polyfill";
+import browser from "webextension-polyfill";
 import { GetOptions } from "./OptionsInterface";
-import { GetTrendingTags } from "./MastodonAPI";
+import { GetFollowedTags, GetTrendingTags } from "./MastodonAPI";
 import { MessageTypes } from "./BackgroundMessages";
 
 async function refreshTrending() {
-    console.log("Refreshing...");
     const { instance } = await GetOptions();
     browser.storage.local.set({ cacheTrending: await GetTrendingTags(instance) });
 }
 
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+async function refreshFollowed() {
+    const { instance, accessKey } = await GetOptions();
+    browser.storage.local.set({ cacheFollowed: await GetFollowedTags(instance, accessKey) });
+}
+
+browser.runtime.onMessage.addListener((message, sender) => {
     console.log("Got message ", message);
     if (message.type === MessageTypes.REFRESH_TRENDING) refreshTrending();
+    if (message.type === MessageTypes.REFRESH_FOLLOWED) refreshFollowed();
 
     if (message.type === MessageTypes.LOAD_SCRIPT) {
         if (sender.tab) browser.tabs.executeScript(sender.tab.id, { file: "mastodon.bundle.js" });
