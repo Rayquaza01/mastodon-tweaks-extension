835e5f45f5c26f51780379f9247fce1e5d8ff15e
Joe Jarvis <joe.jarvis109@gmail.com>
2023-07-06 22:24:38 -0400

    Comment hooks

--- src/js/Options/webext-hooks.tsx
+++ src/js/Options/webext-hooks.tsx
@@ -2,6 +2,12 @@ import browser from "webextension-polyfill";
 import { GetOptions, Options, defaultOptions } from "../OptionsInterface";
 import { useState, useEffect } from "react";
 
+/**
+ * Wrapper for Options as a hook.
+ *
+ * First value is the current options state or default if not set
+ * Second value lets you update the options partially
+ */
 export function useWebextensionOptions(): [Options, (value: Partial<Options>) => void] {
     const [options, setOptions] = useState(defaultOptions);
 
@@ -20,6 +26,11 @@ export function useWebextensionOptions(): [Options, (value: Partial<Options>) =>
     return [options, setStorageValue];
 }
 
+/**
+ * Hook that provides a value from webextension storage as a readonly state variable. Updates state whenever value changes in storage
+ * @param key The key from storage to check
+ * @param defaultValue The default value to fall back to if key is missing
+ */
 export function useWebextensionStorage<T>(key: string, defaultValue: T): T {
     const [val, setVal] = useState(defaultValue);
 
